1393b45bb80dbe84be09f6c3d218ab654405962f
Vitor Gusmao <43387517+AlmostFamouS2@users.noreply.github.com>
2023-05-08 00:34:15 -0300

    Update ceasar.c

--- ceasar.c
+++ ceasar.c
@@ -2,22 +2,22 @@
 #include <string.h>
 
 char cript(char letter){
-	if((letter > 87 && letter < 91) || (letter < 123 && letter > 119))
-	{
-		return letter-=23;
-	}
-	return letter+3;
+        if((letter > 87 && letter < 91) || (letter < 123 && letter > 119))
+                return letter-23;
+
+        if (letter >= '7' && letter < 0x40)
+                return letter -= 6;
+        return letter+3;
 }
 
 int main(int argc, char *argv[]){
 
 for(short v=1;v<argc;v++){
-	short len=strlen(argv[v]);
+        short len=strlen(argv[v]);
 
 for(short i=0;i<len;i++)
-{
-	printf("%c ", cript(argv[v][i]));
-}
+        printf("%c", cript(argv[v][i]));
+	
 printf(" ");
 }
 puts("");
